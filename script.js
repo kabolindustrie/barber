78f347a79633095db177d82c64a28dff70989ef9
Kabol Amine <amine.kabol@gmail.com>
2024-03-26 15:35:43 +0100

    btn toogle de la navbar

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // toogle btn
 
-const nav= document.querySelector('.main-nav');
-const navToggler= document.querySelector('.nav-toggler');
-const togglerImg= document.querySelector('.img');
+const nav = document.querySelector(".main-nav");
+const navToggler = document.querySelector(".nav-toggler");
+const togglerImg = navToggler.querySelector("img");
+const navLinks = document.querySelectorAll(".main-nav a");
+
+navToggler.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  nav.classList.toggle("active");
+  if(togglerImg.src.includes("hamburger")) {
+    togglerImg.src = "ressources/cross.svg";
+    navToggler.ariaExpanded = true;
+  }
+  else {
+    togglerImg.src = "ressources/hamburger.svg";
+    navToggler.ariaExpanded = false;
+  }
+}
+
